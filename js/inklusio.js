7157a9d7d0dd3e61a55b599b778143fa4cf883bb
2biazdk <tobias@2biaz.dk>
2023-12-10 10:51:26 +0100

    Improve WAI-ARIA of language switcher button

--- js/inklusio.js
+++ js/inklusio.js
@@ -1,11 +1,10 @@
 /**
   Script to mimic Bootstraps toggle functionality
 */
-
 function toggle(el) {
   const btn = el;
-  const targetId = btn.getAttribute('data-target');
-  const target = document.querySelector(targetId);
+  const targetId = btn.getAttribute('aria-controls');
+  const target = document.querySelector('#' + targetId);
 
   let isExpanded = btn.getAttribute('aria-expanded') === 'true';
 
@@ -25,24 +24,3 @@ function toggle(el) {
 
 const allTogglers = Array.from(document.querySelectorAll('[data-toggle]'));
 allTogglers.forEach(toggle);
-
-
-// Function for DropDown
-function setupDropdown(id) {
-  var dropdown = document.getElementById(id);
-  var menu = dropdown.querySelector('.dropdown-menu');
-
-  dropdown.addEventListener('click', function () {
-    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
-  });
-
-  document.addEventListener('click', function (event) {
-    if (!dropdown.contains(event.target)) {
-      menu.style.display = 'none';
-    }
-  });
-}
-
-// Call the function with the IDs of the dropdown elements
-setupDropdown('languageDropdownMobile');
-setupDropdown('languageDropdownDesktop');
