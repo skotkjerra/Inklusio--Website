bc1c37d3f0fb403d98caeffbfdf7042146bf20c5
2biazdk <tobias@2biaz.dk>
2024-01-26 16:20:53 +0100

    Remove support for very large images

--- config/imagePlugin.js
+++ config/imagePlugin.js
@@ -3,7 +3,7 @@ const Image = require("@11ty/eleventy-img");
 module.exports = {
   asyncImage: async function(src, alt, sizes) {
     let metadata = await Image(src, {
-      widths: [400, 800, 1200],
+      widths: [400, 800],
       formats: ["png", "jpeg"],
       outputDir: '_site/assets/images',
       urlPath: '/assets/images',
